576cc453ce1e1091f5b9eda5bf80aa860ecf739c
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2023-01-23 03:35:18 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,7 +1,7 @@
 !#/bin/bash
 
 # Install dotfiles with yadm
-yadm clone -f https://gitea.mishracloud.com/Dr.PranavMishra/dotfiles
+yadm clone --recurse-submodules -f https://gitea.mishracloud.com/Dr.PranavMishra/dotfiles
 
 # Change timezone to Chicago
 ls -l /etc/localtime
