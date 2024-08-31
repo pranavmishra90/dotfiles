20773d2dd07cdd084b474680ada8dccbf5df622a
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2024-08-31 15:25:55 -0500

    fix: remove redundant bootstrap file

--- yadm_initialize.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Ensure the script is executed from the root of the repository
-cd "$(dirname "$0")"
-
-# Initialize and update all submodules
-git submodule update --init --recursive
