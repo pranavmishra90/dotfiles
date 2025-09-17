95a67096c0a8dca02a8b47bdde759223d6bc453f
Pranav Kumar Mishra <62562712+pranavmishra90@users.noreply.github.com>
2025-09-17 16:58:30 -0500

    feat: add rich print

--- yadm/functions/active_python.sh
+++ yadm/functions/active_python.sh
@@ -2,11 +2,12 @@
 
 
 function active-python(){
-  uv run -- python - <<'PY'
-  import sys, os, pathlib
-  print("uv sys.executable:", sys.executable)
-  print("uv venv root:", pathlib.Path(sys.executable).parent.parent)
-  print("uv VIRTUAL_ENV:", os.environ.get("VIRTUAL_ENV"))
-  print("uv CONDA_PREFIX:", os.environ.get("CONDA_PREFIX"))
-  PY
+uv run -- python - <<'PY'
+import sys, os, pathlib
+from rich import print
+print("uv sys.executable:", sys.executable)
+print("uv venv root:", pathlib.Path(sys.executable).parent.parent)
+print("uv VIRTUAL_ENV:", os.environ.get("VIRTUAL_ENV"))
+print("uv CONDA_PREFIX:", os.environ.get("CONDA_PREFIX"))
+PY
 }
